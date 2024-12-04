b178f63b3c5908212553d6595460f34e8d948b14
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-12-04 20:13:03 +0200

    Initial Commit

---
+++ Projecr-28-Touch-Face-security/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projecr-28-Touch-Face-security
+//
+//  Created by Serhii Prysiazhnyi on 04.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
